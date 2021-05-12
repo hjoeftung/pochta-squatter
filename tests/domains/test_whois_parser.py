847dead5be249031bea5dbd0770effc1a2ba54a2
hjoeftung <sto-ton@yandex.ru>
2021-05-13 02:53:18 +0300

    start making tests

--- tests/domains/test_whois_parser.py
+++ tests/domains/test_whois_parser.py
@@ -17,6 +17,18 @@ def test_prepare_url():
     for https_url in https_test_urls:
         assert whois_parser.prepare_url(https_url) == https_url[8:]
 
+    # Test that function returns whole url if it does not match the pattern
+    assert whois_parser.prepare_url("unknown_pattern") == "unknown_pattern"
+
 
 def test_get_whois_record():
-    pass
+    assert whois_parser.get_whois_record("https://nonexisting.url") == {
+        "domain_name": "https://nonexisting.url", "owner_name": "",
+        "registrar_name": "", "abuse_emails": ""
+    }
+    assert whois_parser.get_whois_record("https://почта.рф")["owner_name"] == (
+        "JSC Russian Post"
+    )
+    assert whois_parser.get_whois_record("http://почта.рф")[
+               "registrar_name"
+           ] == "RUCENTER-RF"
